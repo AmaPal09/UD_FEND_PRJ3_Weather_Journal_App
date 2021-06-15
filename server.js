419439d800d9b23c2f6eada4735674272a95e71b
Amarja Palekar <amarjapalekar09@gmail.com>
2021-06-15 16:31:05 -0700

    Feat: Set-up the server using express
    Create a server instance
    Require important packages and links to the server

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// File for server side code
+//Server side JS code
+/*All required packages*/
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Start and instance of the app
+const app = express();
+
+/*Middleware
+	Configure express to use body-parser as middle-ware
+*/
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// User cors for cross origin allowance
+app.use(cors());
+
+// Set a port to run the server on
+const port = 3000;
+// Keep the server running and listen for activity
+app.listen(port, ()=>{
+	console.log(`Server running on port: ${port}`);
+});
