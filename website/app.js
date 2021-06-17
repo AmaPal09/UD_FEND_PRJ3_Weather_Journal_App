5bb5104cbeaf6168b2974c6423a702c54e4b1ab3
Amarja Palekar <amarjapalekar09@gmail.com>
2021-06-17 15:34:33 -0700

    Feat: Send POST request from client side

--- website/app.js
+++ website/app.js
@@ -1,1 +1,26 @@
-// Client side code
+const postData = async (url = '', data = {}) => {
+	console.log(data);
+	const response = await fetch( url, {
+		method: 'POST',
+		credentials: 'same-origin',
+		headers: {
+          'Content-Type': 'application/json',
+		},
+		// Body data type must match "Content-Type" header
+		body: JSON.stringify(data),
+	});
+
+	try {
+		console.log("Processing response");
+		const newData = await response.json();
+		console.log("Got response");
+		console.log(newData);
+	    return newData;
+	}catch(error) {
+		console.log("error", error);
+	}
+}
+
+
+// POST request sent to the server
+postData('/addRating', {ratingScore:2});
