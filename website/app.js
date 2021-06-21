d4b21335028088c5196ed531efc71b7e1ef7198b
Amarja Palekar <amarjapalekar09@gmail.com>
2021-06-20 23:24:48 -0700

    Feat: Fetch request to get weather data from open weather maps

--- website/app.js
+++ website/app.js
@@ -25,3 +25,24 @@ const postData = async (url = '', data = {}) => {
 // POST request sent to the server
 postData('/addRating', {temparature: 22, date: '10/10/2021',
 						userResponse: 'happy'});
+
+const getWeatherData = async () => {
+	const url =
+	'api.openweathermap.org/data/2.5/weather?zip=94041&appid=3da249315989972b747443d739018cd3';
+
+	const response = await fetch(url);
+	try {
+		console.log("Processing fetch response");
+		console.log(response);
+		const weatherData = await response.json();
+		// const weatherData = await JSON.parse(response);
+		// const weatherData = await res.text();
+		console.log(weatherData);
+		return weatherData;
+	}
+	catch(error) {
+		console.log("error", error);
+	}
+}
+
+getWeatherData();
