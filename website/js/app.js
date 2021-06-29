8349e2d143de98cd71541c3300917d5c4bf8b39e
Amarja Palekar <amarjapalekar09@gmail.com>
2021-06-29 14:20:55 -0700

    Doc: Remove commented code

--- website/js/app.js
+++ website/js/app.js
@@ -8,7 +8,6 @@ const WEATHER_API_KEY = '&appid=3da249315989972b747443d739018cd3&units=imperial'
 const zip = document.getElementById('zip');
 const feelings = document.getElementById('feeling');
 const generateBtn = document.getElementById('generate');
-// const errorBoard = document.getElementById('error__msg');
 const errorBoard = document.querySelector('.error__box');
 const errorMsg = document.getElementById('error__msg');
 const logBoard = document.querySelector('.weather__log');
